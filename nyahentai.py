ad97d89fe2360a2963ea7ba11ec2def1329bf69f
niming <niming@xinqiwan.com>
2020-11-16 19:52:22 +0800

    修改了写错的header，绕开了反爬

--- nyahentai.py
+++ nyahentai.py
@@ -7,16 +7,17 @@ import re
 _PLATFORM = platform.system()
 #使用此header伪装为chrome访问
 headersWin = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.84 Safari/537.36'}
-headersMac = {'User-Agent': 'User-Agent:Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.84 Safari/537.36'} 
+headersMac = {'User-Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.198 Safari/537.36'} 
+
 def chromeReq(url):
     if (_PLATFORM == 'Darwin'):
         headers = headersMac
     elif(_PLATFORM == 'Windows'):
         headers = headersWin
-    print(_PLATFORM)
+
     req = urllib2.Request(url,headers = headers)
     response = urllib2.urlopen(req)
-    print(response.read())
+    
     return response.read()
     
 
@@ -32,9 +33,10 @@ def openIDPage(mID):
 def openArtistPage(mArtist):
     mArtist = mArtist.replace(' ','-')
     url = r'https://nyahentai.cc/artist/'+mArtist+r'/'
+    # url = r'https://nyahentai.cc'
     print('open_url: '+url)
     res = chromeReq(url)
-    
+    print(res)
 # def download_img(img_url,totalpage):
 
 #     for index in range(1,totalpage):
