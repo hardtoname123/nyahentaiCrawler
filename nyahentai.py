b616cfda085c083a06d515374087e3bfa73fa940
niming <niming@xinqiwan.com>
2021-01-14 16:45:30 +0800

    下载(mac平台)

--- nyahentai.py
+++ nyahentai.py
@@ -3,7 +3,7 @@
 
 import urllib2
 import platform
-import re
+import re,os
 import stringUtil as su
 _PLATFORM = platform.system()
 #使用此header伪装为chrome访问
@@ -15,7 +15,6 @@ def chromeReq(url):
         headers = headersMac
     elif(_PLATFORM == 'Windows'):
         headers = headersWin
-
     req = urllib2.Request(url,headers = headers)
     response = urllib2.urlopen(req)
     return response.read()
@@ -32,36 +31,47 @@ def openIDPage(mID):
 
 def openArtistPage(mArtist):
     mArtist = mArtist.replace(' ','-')
+    path = r'/Users/mingni/Documents/myCrawler/nyahentaiCrawler'
     url = target_url+r'artist/'+mArtist+r'/'
     # url = r'https://nhentai.to'
-    # print('open_url: '+url)
+    # print('openUrl: '+url)
     regs = r'<a href=.+?class="cover"'
-    print("进入到"+mArtist+"页面:"+url)
+    print("获取"+mArtist+"页面:"+url)
     res = chromeReq(url)
     print("进行Artist页面解析")
     matchRes = su.string_match(res,regs)
-    regs = r'g/(\w)+?/'
-    onceFlag = True
+    urlregs = r'g/(\w)+?/'
+    onceFlag = 1
+    suffname = ""
     for x in matchRes:
-        search = su.string_search(x,regs)
-        open_url = target_url+search.group(0)
-        if (onceFlag == True):
-            
-            workRes = openOneWork(open_url)
-            onceFlag = False
-        
+        search = su.string_search(x,urlregs)
+        dirname = su.string_search(x,r'[0-9]+').group(0)
+        openUrl = target_url+search.group(0)
+        if (onceFlag <= 2):
+            workRes = getWorkResUrl(openUrl)
+            suffname = getImgType(workRes[0])
+            onceFlag = onceFlag + 1
+            downloadImg(path,workRes,dirname,suffname)
 
-def openOneWork(url):
-    print("进入到数据页面:"+url)
+def getWorkResUrl(url):
+    print("获取作品内容页面:"+url)
     res = chromeReq(url)
-    regs = r'data-src="https.+?png"'
+    regs = r'data-src="https.+?"'
     print("进行数据页面解析")
     matchRes = su.string_match(res,regs)
+    urlregs = r'https:.+?(png|jpg)'
+    dataList = []
     for x in matchRes:
-        print(x)
-    return res
-# def download_img(img_url,totalpage):
+        matchData = su.string_search(x,urlregs)
+        dataList.append(matchData.group(0))
+    return dataList
 
+def getImgType(imgUrl):
+    if imgUrl[-3] == 'p':
+        return r"png"
+    elif imgUrl[-3] == 'j':
+        return r"jpg"
+# def downloadImg(img_url,totalpage):
 #     for index in range(1,totalpage):
 #         img_url_tmp = img_url+str(index)+'.jpg'
 #         #img_url_tmp = img_url+'list/'+str(index)+'/#pagination-page-top'
@@ -73,7 +83,21 @@ def openOneWork(url):
 #         f.write(res)
 #         f.close()
 
-    
+def downloadImg(path,urlList,dirname,suffname):
+    newPath = path+r'/'+dirname+r'/'
+    if not os.path.exists(newPath):
+        os.mkdir(newPath)
+    picCount = 1
+    for x in urlList:
+        if True:
+            res = chromeReq(x)
+            filePath = newPath+str(picCount)+r'.'+suffname
+            print("获取图片数据:"+filePath)
+            f = open(filePath , "wb")
+            f.write(res)
+            f.close
+            print("图片获取成功")
+            picCount = picCount + 1
 
 def runDebug():
     # chromeReq('https://tool.lu')
